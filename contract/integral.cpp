f7ef6d488a17240bfc91d835d92a6e2c92bc04bf
wyx <Max_Wang.cs@foxmail.com>
2021-02-26 23:44:21 +0800

    update

--- contract/integral.cpp
+++ contract/integral.cpp
@@ -1,7 +1,6 @@
 #include<eosio/eosio.hpp>
 #include<eosio/asset.hpp>
 #include<string>
-#include<stdio.h>
 
 using namespace eosio;
 using std::string;
@@ -94,35 +93,63 @@ public:
 	}
 
 	 [[eosio::action]]
-    void transfer( const name& owner, const asset& from, const symbol& to, const string& memo ){
+    void transfer( const name&    from, const name&    to, const asset&   quantity, const string&  memo  ){
     	/*
 		@param owner - the account to transfer integral,
         @param from - the integral to transferr from,
         @param to - the integral to be transferred,
         @param memo - the memo string to accompany the transaction.
     	*/
-    	printf("debug");
-	    check( from.symbol == to, "symbol precision mismatch" );
-	    require_auth( owner );
-	    check( is_account( owner ), "owner account does not exist");
-	    auto sym_to = to.code();
-	    auto sym_from = from.symbol.code();
-	    stats statstable_to( get_self(), sym_to.raw() );
-	    stats statstable_from( get_self(), sym_from.raw() );
-	    const auto& st_to = statstable_to.get( sym_to.raw() );
-	    const auto& st_from = statstable_from.get( sym_from.raw() );
+    	check( from != to, "cannot transfer to self" );
+	    require_auth( from );
+	    check( is_account( to ), "to account does not exist");
+	    auto sym = quantity.symbol.code();
+	    stats statstable( get_self(), sym.raw() );
+	    const auto& st = statstable.get( sym.raw() );
 
-	    require_recipient( owner );
+	    require_recipient( from );
+	    require_recipient( to );
 
-	    check( from.is_valid(), "invalid quantity" );
-	    check( from.amount > 0, "must transfer positive quantity" );
-	    check( from.symbol == st_from.supply.symbol, "symbol precision mismatch");
-	    check( to == st_to.supply.symbol, "symbol precision mismatch");
+	    check( quantity.is_valid(), "invalid quantity" );
+	    check( quantity.amount > 0, "must transfer positive quantity" );
+	    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
 	    check( memo.size() <= 256, "memo has more than 256 bytes" );
 
-	    sub_balance( owner, from );
-	    add_balance( owner, from, to );
+	    auto payer = has_auth( to ) ? to : from;
+
+	    sub_balance( from, quantity );
+	    add_balance( to, quantity, payer );
     }
+
+	 [[eosio::action]]
+    void open( const name& owner, const symbol& symbol, const name& ram_payer ){
+   		require_auth( ram_payer );
+
+	    check( is_account( owner ), "owner account does not exist" );
+
+	    auto sym_code_raw = symbol.code().raw();
+	    stats statstable( get_self(), sym_code_raw );
+	    const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+	    check( st.supply.symbol == symbol, "symbol precision mismatch" );
+
+	    accounts acnts( get_self(), owner.value );
+	    auto it = acnts.find( sym_code_raw );
+	    if( it == acnts.end() ) {
+	        acnts.emplace( ram_payer, [&]( auto& a ){
+	        	a.balance = asset{0, symbol};
+	        });
+	    }
+	}
+
+	 [[eosio::action]]
+	void close( const name& owner, const symbol& symbol ){
+  		require_auth( owner );
+	   	accounts acnts( get_self(), owner.value );
+	   	auto it = acnts.find( symbol.code().raw() );
+	   	check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+	   	check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+	   	acnts.erase( it );
+	}
 
 private:
     struct [[eosio::table]] account {
@@ -151,19 +178,6 @@ private:
 	    from_acnts.modify( from, owner, [&]( auto& a ) {
 	         a.balance -= value;
 	      });
-	}
-    void add_balance( const name& owner, const asset& value, const symbol& to_symbol ){
-	    accounts to_acnts( get_self(), owner.value );
-	    auto to = to_acnts.find( to_symbol.code().raw() );
-	    if( to == to_acnts.end() ) {
-	    	to_acnts.emplace( owner, [&]( auto& a ){
-	        	a.balance = value;
-	      	});
-	   } else {
-	      	to_acnts.modify( to, same_payer, [&]( auto& a ) {
-	        	a.balance += value;
-	      	});
-	   }
 	}
 	void add_balance( const name& owner, const asset& value, const name& ram_payer ){
 	    accounts to_acnts( get_self(), owner.value );
