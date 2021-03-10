e5ba28890626352e54f41073098fb04fa526cd72
Patrick Raphael <praphael@gmail.com>
2021-03-10 17:45:01 +0000

    added 'likepost' feature, updated front end

--- contract/talk.cpp
+++ contract/talk.cpp
@@ -1,4 +1,9 @@
 #include <eosio/eosio.hpp>
+#include <eosio/crypto.hpp>
+
+// enum react_t { NONE, LIKE, LOVE, LOL, WOW, SAD };
+using react_t = uint8_t;
+constexpr react_t REACT_TYPE_MAX = 5;
 
 // Message table
 struct [[eosio::table("message"), eosio::contract("talk")]] message {
@@ -11,8 +16,32 @@ struct [[eosio::table("message"), eosio::contract("talk")]] message {
     uint64_t get_reply_to() const { return reply_to; }
 };
 
+// Reaction table
+struct [[eosio::table("likes"), eosio::contract("talk")]] likes {
+    uint64_t    id       = {}; // Non-0
+    uint64_t    reply_to = {}; // Non-0 
+    eosio::name user     = {};
+    react_t react        = {};  // reaction type
+
+    uint64_t primary_key() const { return id; }
+    uint64_t get_reply_to() const { return reply_to; }
+};
+
 using message_table = eosio::multi_index<
-    "message"_n, message, eosio::indexed_by<"by.reply.to"_n, eosio::const_mem_fun<message, uint64_t, &message::get_reply_to>>>;
+    "message"_n, message, 
+    eosio::indexed_by<
+        "by.reply.to"_n, 
+        eosio::const_mem_fun<message, uint64_t, &message::get_reply_to>
+    >
+>;
+
+using likes_table = eosio::multi_index<
+    "likes"_n, likes, 
+    eosio::indexed_by<
+        "by.reply.to"_n, 
+        eosio::const_mem_fun<likes, uint64_t, &likes::get_reply_to>
+    >
+>;
 
 // The contract
 class talk : eosio::contract {
@@ -28,9 +57,10 @@ class talk : eosio::contract {
         require_auth(user);
 
         // Check reply_to exists
-        if (reply_to)
+        if (reply_to) {
             table.get(reply_to);
-
+        }
+        
         // Create an ID if user didn't specify one
         eosio::check(id < 1'000'000'000ull, "user-specified id is too big");
         if (!id)
@@ -43,5 +73,61 @@ class talk : eosio::contract {
             message.user     = user;
             message.content  = content;
         });
+    }
+
+    // Post a message
+    [[eosio::action]] void likepost(uint64_t reply_to, eosio::name user, react_t react) {
+        message_table msg{get_self(), 0};
+        auto likes = likes_table{get_self(), 0};
+
+        // Check user
+        require_auth(user);
+
+        eosio::check(react <= REACT_TYPE_MAX, "Bad value for reaction");
+
+        // if there is reaction reply_to must be non-zero
+        eosio::check(reply_to > 0, "Must have valid reply-to");
+
+        // Check reply_to exists
+        auto it_msg = msg.find(reply_to);
+        
+        // cannot like your own post
+        eosio::check(it_msg->user != user, "Cannot like yur own post");
+
+        // determien if already liked this post
+        auto idx = likes.get_index<"by.reply.to"_n>();
+        auto it = idx.find(reply_to);
+        if (it != idx.end()) {
+            while(it != idx.end() && it->reply_to == reply_to && it->user != user) {
+                it++;
+            }
+        }
+        // if reaction is 0, post must already exist
+        if(react == 0)
+            eosio::check(it != idx.end() && it->user == user && it->reply_to == reply_to, "Like not found in table");
+
+        // like does not exist, so insert new record
+        if (it == idx.end() || it->reply_to != reply_to || it->user != user) {
+            likes.emplace(get_self(), [&](auto& like) {
+                like.id       = likes.available_primary_key();
+                like.reply_to = reply_to;
+                like.user     = user;
+                like.react    = react;
+            });
+        }  
+        // othwerise like already exists in talbe, so either erase it if reaction is 0 or update it
+        else {
+            auto like_it = likes.find(it->id);
+            if (react == 0) {
+                likes.erase(like_it);
+            } else {
+                likes.modify(like_it, get_self(), [&](auto& like) {
+                    like.reply_to = reply_to;
+                    like.user     = user;
+                    like.react    = react;
+                });
+            }
+        }
+
     }
 };
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,4 +1,4 @@
-// To see this in action, run this in a terminal:
+/// To see this in action, run this in a terminal:
 //      gp preview $(gp url 8000)
 
 import * as React from "react";
@@ -15,11 +15,21 @@ interface PostData {
     content?: string;
 };
 
+interface LikePostData {
+    id? :number;
+    user?: string;
+    reply_to?: number;
+    react?: number;
+};
+
 interface PostFormState {
     privateKey: string;
     data: PostData;
+    likeData: LikePostData;
     error: string;
+    likeError: string;
 };
+
 
 class PostForm extends React.Component<{}, PostFormState> {
     api: Api;
@@ -33,14 +43,25 @@ class PostForm extends React.Component<{}, PostFormState> {
                 id: 0,
                 user: 'bob',
                 reply_to: 0,
-                content: 'This is a test'
+                content: 'This is a test',
+            },
+            likeData: {
+                id: 0,
+                user: 'bob',
+                reply_to: 0,
+                react: 1,
             },
             error: '',
+            likeError: '',
         };
     }
 
     setData(data: PostData) {
         this.setState({ data: { ...this.state.data, ...data } });
+    }
+
+    setLikeData(likeData: LikePostData) {
+        this.setState({ likeData: { ...this.state.likeData, ...likeData } });
     }
 
     async post() {
@@ -68,6 +89,34 @@ class PostForm extends React.Component<{}, PostFormState> {
                 this.setState({ error: JSON.stringify(e.json, null, 4) });
             else
                 this.setState({ error: '' + e });
+        }
+    }
+
+    async like() {
+        try {
+            this.api.signatureProvider = new JsSignatureProvider([this.state.privateKey]);
+            const result = await this.api.transact(
+                {
+                    actions: [{
+                        account: 'talk',
+                        name: 'likepost',
+                        authorization: [{
+                            actor: this.state.likeData.user,
+                            permission: 'active',
+                        }],
+                        data: this.state.likeData,
+                    }]
+                }, {
+                    blocksBehind: 3,
+                    expireSeconds: 30,
+                });
+            console.log(result);
+            this.setState({ likeError: '' });
+        } catch (e) {
+            if (e.json)
+                this.setState({ likeError: JSON.stringify(e.json, null, 4) });
+            else
+                this.setState({ likeError: '' + e });
         }
     }
 
@@ -116,6 +165,41 @@ class PostForm extends React.Component<{}, PostFormState> {
                 Error:
                 <code><pre>{this.state.error}</pre></code>
             </div>}
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td>User</td>
+                        <td><input
+                            style={{ width: 500 }}
+                            value={this.state.likeData.user}
+                            onChange={e => this.setLikeData({ user: e.target.value })}
+                        /></td>
+                    </tr>
+                    <tr>
+                        <td>Reply To</td>
+                        <td><input
+                            style={{ width: 500 }}
+                            value={this.state.likeData.reply_to}
+                            onChange={e => this.setLikeData({ reply_to: +e.target.value })}
+                        /></td>
+                    </tr>
+                    <tr>
+                        <td>Reaction</td>
+                        <td><input
+                            style={{ width: 500 }}
+                            value={this.state.likeData.react}
+                            onChange={e => this.setLikeData({ react: +e.target.value })}
+                        /></td>
+                    </tr>
+                </tbody>
+            </table>
+            <button onClick={e => this.like()}>Like</button>
+            {this.state.likeError && <div>
+                <br />
+                Error:
+                <code><pre>{this.state.likeError}</pre></code>
+            </div>}
         </div>;
     }
 }
@@ -135,14 +219,57 @@ class Messages extends React.Component<{}, { content: string }> {
                     json: true, code: 'talk', scope: '', table: 'message', limit: 1000,
                 });
                 let content =
-                    'id          reply_to      user          content\n' +
-                    '=============================================================\n';
+                    'id          reply_to      user          content                   \n' +
+                    '=======================================================================\n';
                 for (let row of rows.rows)
                     content +=
                         (row.id + '').padEnd(12) +
                         (row.reply_to + '').padEnd(12) + '  ' +
                         row.user.padEnd(14) +
-                        row.content + '\n';
+                        row.content.padEnd(24) + '\n';
+                this.setState({ content });
+            } catch (e) {
+                if (e.json)
+                    this.setState({ content: JSON.stringify(e.json, null, 4) });
+                else
+                    this.setState({ content: '' + e });
+            }
+
+        }, 200);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        return <code><pre>{this.state.content}</pre></code>;
+    }
+}
+
+class Likes extends React.Component<{}, { content: string }> {
+    interval: number;
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { content: '///' };
+    }
+
+    componentDidMount() {
+        this.interval = window.setInterval(async () => {
+            try {
+                let content =
+                    'id          reply_to      user          reaction                   \n' +
+                    '===================================================================\n';
+                const likeRows = await rpc.get_table_rows({
+                    json: true, code: 'talk', scope: '', table: 'likes', limit: 1000,
+                });                   
+                for (let row of likeRows.rows)
+                    content +=
+                        (row.id + '').padEnd(12) +
+                        (row.reply_to + '').padEnd(12) + '  ' +
+                        row.user.padEnd(14) +
+                        (row.react + '').padEnd(14) + '\n';                    
                 this.setState({ content });
             } catch (e) {
                 if (e.json)
@@ -169,6 +296,8 @@ ReactDOM.render(
         <br />
         Messages:
         <Messages />
+        Likes:
+        <Likes />
     </div>,
     document.getElementById("example")
 );
