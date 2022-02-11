2eb70bb7f18c77add96975b568bd01bf82b4263a
Previn Wong <pwong@esri.com>
2022-02-11 00:54:54 +0000

    y

--- contract/tctest.cpp
+++ contract/tctest.cpp
@@ -2,12 +2,15 @@
 
 namespace eosio {
 
-void token::create()
+void token::create( const name&   issuer,
+                    const asset&  maximum_supply )
 {
     require_auth( get_self() );
 
-auto sym = symbol("TERROR", 4);
-     auto maximum_supply = asset(210000000000, sym);
+    auto sym = maximum_supply.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( maximum_supply.is_valid(), "invalid supply");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
 
     stats statstable( get_self(), sym.code().raw() );
     auto existing = statstable.find( sym.code().raw() );
